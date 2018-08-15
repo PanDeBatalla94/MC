f7880274da0f46f9430a65c0c40c81f6f892fb55
Maria Canqui <mariarenee.canqui@gmail.com>
2018-08-15 19:29:07 -0400

    added argument to deploy gradle

--- gradle/quickstart-web/deploy-app.sh
+++ gradle/quickstart-web/deploy-app.sh
@@ -1,12 +1,12 @@
-
+WAR_NAME=$1
 echo 'Stopping Tomcat service...'
 sudo systemctl stop tomcat8
-sudo rm -f ${tomcat_webapps}/${war_name}.war
-sudo rm -rf ${tomcat_webapps}/${war_name}
+sudo rm -f ${tomcat_webapps}/$WAR_NAME.war
+sudo rm -rf ${tomcat_webapps}/$WAR_NAME
 
 echo 'Sending war file...'
-sudo cp -f /tmp/${war_name}.war ${tomcat_webapps}/${war_name}.war
-sudo rm /tmp/${war_name}.war
+sudo cp -f /tmp/$WAR_NAME.war ${tomcat_webapps}/$WAR_NAME.war
+sudo rm /tmp/$WAR_NAME.war
 
 echo 'Starting Tomcat service...'
-sudo 'systemctl start tomcat8'
+sudo systemctl start tomcat8
