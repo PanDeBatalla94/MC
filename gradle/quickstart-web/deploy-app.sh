3503c7ca9916cbf733e5167764978c0c8da38d8e
Maria Canqui <mariarenee.canqui@gmail.com>
2018-08-15 18:55:48 -0400

    change command to execute sh

--- gradle/quickstart-web/deploy-app.sh
+++ gradle/quickstart-web/deploy-app.sh
@@ -6,7 +6,7 @@ sudo rm -rf ${tomcat_webapps}/${war_name}
 
 echo 'Sending war file...'
 sudo cp -f /tmp/${war_name}.war ${tomcat_webapps}/${war_name}.war
-sudo rm -f /tmp/${war_name}.war
+sudo rm /tmp/${war_name}.war
 
 echo 'Starting Tomcat service...'
 sudo 'systemctl start tomcat8'
